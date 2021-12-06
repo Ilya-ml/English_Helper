12adf0ee68882c085fd54de28e589962d66dcff7
Ilya-ml <melnikilya58@gmail.com>
2021-12-06 13:36:12 +0300

    add begginer, intermediate, pre_intermediate

---
+++ pre_intermediate.py
@@ -0,0 +1,60 @@
+relax = "cartoon - Мультфильм\n"                           \
+        "movie - Фильм\n"                                  \
+        "circus - Цирк\n"                                  \
+        "composer - Композитор\n"                          \
+        "dream	 - Мечта\n"                                \
+        "daily routine - Ежедневная рутина\n"              \
+        "horror	 - Ужастик\n"                              \
+        "laughter - Смех\n"                                \
+        "novel	 - Роман\n"                                \
+        "story	 - История\n"                              \
+        "weather forecast - Прогноз погоды\n"              \
+        "thought - Мысль\n"                                \
+        "writer - Писатель\n"                              \
+        "access - Доступ\n"                                \
+        "account - Аккаунт\n"                              \
+        "adventure - Приключение\n"                        \
+        "affair - Страх\n"                                 \
+        "anniversary - Дело, интрига\n"                    \
+        "entertainment - Развлечение\n"                    \
+        "amusement	 - Веселье\n"                          \
+        "influence	 - Влияние\n"                          \
+        "attraction - Достопримечательность, аттракцион\n" \
+        "rock-climbing	 - Скалолазание\n"                 \
+        "cycling - Катание на велосипеде\n"                \
+        "number painting - Живопись по номерам\n"          \
+        "swimming pool - Бассейн\n"                        \
+        "yoga - Йога\n"                                    \
+        "coffee shop - Кофейня\n"                          \
+        "central park - Центральный парк\n"                \
+        "inspiration - Вдохновение"
+
+
+climate = "cactivist - Активист\n"                  \
+          "pollution - Загрязнение\n"               \
+          "impurity - Примеси, загрязнение\n"       \
+          "algae - Водоросли\n"                     \
+          "Earth - Земля\n"                         \
+          "atmosphere - Атмосфера\n"                \
+          "environment - Окружающий мир\n"          \
+          "climate - Климат\n"                      \
+          "air - Воздух\n"                          \
+          "water reserve - Водные ресурсы\n"        \
+          "greenhouse effect - Парниковый эффект\n" \
+          "oxygen - Кислород\n"                     \
+          "explorer - Исследователь\n"              \
+          "investigation - Расследование\n"         \
+          "future - Будущее\n"                      \
+          "tropic forest - Тропический лес\n"       \
+          "humidity	 - Влажность, влага\n"          \
+          "flora - Флора\n"                         \
+          "wildlife - Дикая природа, животные\n"    \
+          "plastic waste - Пластиковые отходы\n"    \
+          "recycling - Переработка\n"               \
+          "hydrosphere - Гидросфера\n"              \
+          "background - Происхождение, фон\n"       \
+          "bank - Берег\n"                          \
+          "banner - Баннер, реклама\n"              \
+          "beast - Зверь, животное\n"               \
+          "cave - Пещера, грот\n"                   \
+          "caviar - Икра"
